d72ae4b27fc33fbcbd00122d38b59bec5f076f6a
Audrey Maldonado <audrey.maldonado@gmail.com>
2022-04-03 22:02:55 -0400

    secret route added

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/secret')
+def secret_message():
+    return 'This cucumber cries at midnight!'
